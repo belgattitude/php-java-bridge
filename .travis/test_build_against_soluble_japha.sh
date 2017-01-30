0689eafeae3392d2061dff040a27dba804622914
sebastien <belgattitude@gmail.com>
2017-01-30 14:06:31 +0100

    Attempt to fix travis

--- .travis/test_build_against_soluble_japha.sh
+++ .travis/test_build_against_soluble_japha.sh
@@ -49,7 +49,8 @@ install_soluble_japha() {
 }
 
 
-runEmbeddedTomcat() {
+runEmbeddedTomcatDaemon() {
+    # Launch through the embedded gradle tomcat plugin
     cd $PROJECT_DIR
     gradle tomcatRun
 }
@@ -58,7 +59,7 @@ runPHPUnit()  {
     cd $JAPHA_DIR
     echo "[*] Running phpunit"
     cp ../phpunit.travis.xml .
-    phpunit -c ./phpunit.travis.xml
+    ./vendor/bin/phpunit -c ./phpunit.travis.xml -v --debug
 }
 
 stopEmbeddedTomcat() {
@@ -69,8 +70,6 @@ stopEmbeddedTomcat() {
 
 # Here's the steps
 install_soluble_japha;
-runEmbeddedTomcat;
-# Sleep for travis
-sleep 3
+runEmbeddedTomcatDaemon;
 runPHPUnit;
 stopEmbeddedTomcat;
