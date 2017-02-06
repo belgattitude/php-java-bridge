bf2ba82074ced81be3c1a54f7f7473f8d32fa0e5
sebastien <belgattitude@gmail.com>
2017-02-06 17:10:59 +0100

    Removed server resources

--- server/WEB-INF/cgi/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# php fcgi launcher
-#set -x
-
-"$@" 1>&2 &
-trap "kill $! && exit 0;" 1 2 15
-read result 1>&2
-kill $!
--- server/buildJavaBridgeProperties.php
+++
@@ -1,4 +0,0 @@
-<?php
-$version = trim(file_get_contents("../VERSION"));
-file_put_contents($argv[1], preg_replace("|@BACKEND_VERSION@|", $version, file_get_contents($argv[2])));
-?>
